cb980cf4cc51f01081d20880101ae00d27cc356a
KeKs0r <marc.hoeffl@laserfocus.io>
2024-11-14 21:38:27 +0800

    Readme for engage sdk v0

---
+++ repl/examples.ts
@@ -0,0 +1,23 @@
+// Copy paste examples from readme, to make sure they match the types
+
+import { createEngage } from "@numia/engage-sdk";
+
+const analytics = createEngage({
+  app: "soon-unicorn-app",
+  apiKey: "MySecretWriteKey",
+  test: true, // remove this for production
+});
+
+analytics.identify("dydx12345678901234567890", {
+  // Optional Properties to store with the identified wallet
+  wallet: "Metamask",
+});
+
+analytics.track("Order Placed", {
+  market: "BTC-USDC",
+  orderId: "1234567890",
+  amount: 100,
+  currency: "USDC",
+});
+
+analytics.page();
